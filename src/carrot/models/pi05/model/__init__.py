020a8bf7c5d33cb55058ff83e7c72c8b782883e5
nrwu <nrwu@tencent.com>
2026-09-14 23:00:21 +0800

    Vendor OpenPI PyTorch PI0.5 model

--- src/carrot/models/pi05/model/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Native PI0.5 architecture, adapted from verl-vla's implementation."""
-
-from .modeling_pi05 import PI0Policy, make_att_2d_masks
-from .paligemma_with_expert import GemmaDecoderLayerWithExpert
-
-__all__ = ["GemmaDecoderLayerWithExpert", "PI0Policy", "make_att_2d_masks"]
